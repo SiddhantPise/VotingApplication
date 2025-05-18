0aa6f3f58cf7cbabd5fc39202017b5aa68ca455d
Siddhant <siddhant@example.com>
2025-05-18 22:41:07 +0530

    Removed folder-name from tracking

--- New folder/OneDrive-2025-05-14/CODES/Votezy/src/main/java/in/scalive/votezy/repository/VoterRepository.java
+++
@@ -1,9 +0,0 @@
-package in.scalive.votezy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.scalive.votezy.entity.Voter;
-
-public interface VoterRepository extends JpaRepository<Voter,Long> {
-     boolean existsByEmail(String email);
-}
